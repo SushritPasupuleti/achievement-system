e4e246c6d2c74e048d5bcdc91728a540f2d0d4fd
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-27 23:50:27 +0530

    setup basic rules

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Engine } = require('json-rules-engine')
+var bodyParser = require('body-parser')
+const { rules } = require('./rules');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    res.send('Hello There!')
+})
+
+app.get('/get-achievement', (req, res) => {
+    console.log(rules)
+
+    const engine = new Engine()
+
+    engine.addRule(rules)
+
     res.send('Hello There!')
 })
 
