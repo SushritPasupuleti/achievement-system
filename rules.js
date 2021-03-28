2c9ae6be0da1fd3c97fbf63a831982c39ca71000
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-28 13:02:06 +0530

    setup multiple badge rules (user can get more than 1 badge at once if conditions are met)

--- rules.js
+++ rules.js
@@ -7,9 +7,41 @@ module.exports.rules = {
         }]
     },
     event: { // define the event to fire when the conditions evaluate truthy
-        type: 'score',
+        type: '5score',
         params: {
             message: '5 Point badge'
         }
     }
 }
+
+module.exports.rules_list = [{
+        conditions: {
+            all: [{
+                fact: 'score',
+                operator: 'greaterThanInclusive',
+                value: 5
+            }]
+        },
+        event: { // define the event to fire when the conditions evaluate truthy
+            type: '5score',
+            params: {
+                message: '5 Point badge'
+            }
+        }
+    },
+    {
+        conditions: {
+            all: [{
+                fact: 'score',
+                operator: 'greaterThanInclusive',
+                value: 10
+            }]
+        },
+        event: { // define the event to fire when the conditions evaluate truthy
+            type: '10score',
+            params: {
+                message: '10 Point badge'
+            }
+        }
+    }
+]
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const { Engine } = require('json-rules-engine')
 var bodyParser = require('body-parser')
-const { rules } = require('./rules');
+const { rules, rules_list } = require('./rules');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +22,30 @@ app.post('/get-achievement', async(req, res) => {
     const engine = new Engine()
 
     engine.addRule(rules)
+
+    const { events } = await engine.run(facts)
+
+    console.log("events: ", events)
+
+    let results = []
+
+    events.map(event => results.push(event.params.message))
+
+    res.send(results)
+})
+
+app.post('/get-achievements', async(req, res) => {
+    //takes list of rules instead of just one rule
+    console.log(rules_list)
+
+    const facts = {
+        score: req.body.score,
+    }
+
+    const engine = new Engine()
+
+    rules_list.map((rule) => engine.addRule(rule))
+
 
     const { events } = await engine.run(facts)
 
