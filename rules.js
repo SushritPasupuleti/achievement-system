9bedef61696ee86812a4d7e49684bbfdf706a99e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-28 13:03:02 +0530

    Update rules.js

--- rules.js
+++ rules.js
@@ -22,7 +22,7 @@ module.exports.rules_list = [{
                 value: 5
             }]
         },
-        event: { // define the event to fire when the conditions evaluate truthy
+        event: {
             type: '5score',
             params: {
                 message: '5 Point badge'
@@ -37,10 +37,25 @@ module.exports.rules_list = [{
                 value: 10
             }]
         },
-        event: { // define the event to fire when the conditions evaluate truthy
+        event: {
             type: '10score',
             params: {
                 message: '10 Point badge'
+            }
+        }
+    },
+    {
+        conditions: {
+            all: [{
+                fact: 'score',
+                operator: 'greaterThanInclusive',
+                value: 15
+            }]
+        },
+        event: {
+            type: '15score',
+            params: {
+                message: '15 Point badge'
             }
         }
     }
